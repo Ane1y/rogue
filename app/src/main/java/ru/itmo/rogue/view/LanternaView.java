5eecbbb036c8184b6a555976e00c1644e885f708
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-06-05 01:57:38 +0300

    Function for square drawing

--- app/src/main/java/ru/itmo/rogue/view/LanternaView.java
+++ app/src/main/java/ru/itmo/rogue/view/LanternaView.java
@@ -41,29 +41,26 @@ public class LanternaView implements View {
     }
 
     private void drawWindowBorders() {
+        screen.doResizeIfNecessary(); // Actualize size
+        // Border for window
         TerminalSize termSize = screen.getTerminalSize();
-//        while (termSize.getColumns() < LanternaView.MIN_SCREEN_COLUMN || termSize.getRows() < LanternaView.MAX_SCREEN_ROW) {
-//            checkBorders();
-//            screen.doResizeIfNecessary();
-//            termSize = screen.getTerminalSize();
-//        }
+        TerminalSize borderSize = new TerminalSize(
+                termSize.getColumns() - 2,
+                termSize.getRows() - 2);
+        drawSquare(new TerminalPosition(1, 1), borderSize, doubled);
 
+        // Examples:
+        var size = new TerminalSize(5, 5);
+        var pos1 = new TerminalPosition(2, 2);
+        var pos2 = new TerminalPosition(8, 2);
+        var pos3 = new TerminalPosition(2, 8);
+        var pos4 = new TerminalPosition(8, 8);
 
-        for (int column = 0; column < termSize.getColumns(); column++) {
-            screen.setCharacter(column, 0, new TextCharacter('-', TextColor.ANSI.RED,  TextColor.ANSI.DEFAULT));
-            screen.setCharacter(column, termSize.getRows() - 1, new TextCharacter('-', TextColor.ANSI.RED,  TextColor.ANSI.DEFAULT));
-        }
+        drawSquare(pos1, size, simple);
+        drawSquare(pos2, size, bold);
+        drawSquare(pos3, size, dash);
+        drawSquare(pos4, size, doubled);
 
-        for (int row = 0; row < termSize.getRows(); row++) {
-            screen.setCharacter(0, row, new TextCharacter('|', TextColor.ANSI.RED,  TextColor.ANSI.DEFAULT));
-            screen.setCharacter(termSize.getColumns() - 1, row, new TextCharacter('|', TextColor.ANSI.RED, TextColor.ANSI.DEFAULT));
-        }
-        TextGraphics graphics = screen.newTextGraphics();
-        graphics.drawRectangle(new TerminalPosition(2, 2), new TerminalSize(5,5), Character.toChars(0x2591)[0]);
-        // 2588 --- полностью (в фокусе)
-        // 2591 --- рябящий
-
-        drawSquare();
         try {
             screen.refresh(Screen.RefreshType.COMPLETE);
         } catch (IOException e) {
@@ -90,11 +87,68 @@ public class LanternaView implements View {
         NORMAL, BOLD
     }
 
-    static char getUnicode(int unicode) {
+    record SquareChars(char horizontal, char vertical, char[] corners) {
+        public SquareChars(int horizontal, int vertical, int[] corners){
+            this(getUnicode(horizontal), getUnicode(vertical), getUnicode(corners));
+        }
+        /*  1 h h h 2
+         *  v       v
+         *  v       v
+         *  v       v
+         *  3 h h h 4
+         */
+    }
+
+    static SquareChars simple = new SquareChars(
+            0x2500,
+            0x2502,
+            new int[]{0x250C, 0x2510, 0x2514, 0x2518});
+
+    static SquareChars dash = new SquareChars(
+            0x2505,
+            0x2506,
+            new int[]{0x250C, 0x2510, 0x2514, 0x2518});
+
+    static SquareChars bold = new SquareChars(
+            0x2501,
+            0x2503,
+            new int[]{0x250F, 0x2513, 0x2517, 0x251B});
+
+    static SquareChars doubled = new SquareChars(
+            0x2550,
+            0x2551,
+            new int[]{0x2554, 0x2557, 0x255A, 0x255D});
+
+
+    private void drawSquare(TerminalPosition position, TerminalSize size, SquareChars chars) {
+        var graphics = screen.newTextGraphics();
+        var x = position.getColumn();
+        var y = position.getRow();
+        var dx = size.getColumns();
+        var dy = size.getRows();
+
+        // horizontal lines
+        graphics.drawLine(x + 1, y, x + dx - 1, y, chars.horizontal);
+        graphics.drawLine(x + 1, y + dy, x + dx - 1, y + dy, chars.horizontal);
+        // vertical lines
+        graphics.drawLine(x, y + 1, x, y + dy - 1, chars.vertical);
+        graphics.drawLine(x + dx, y + 1, x + dx, y + dy - 1, chars.vertical);
+        // corners
+        graphics.setCharacter(x, y, chars.corners[0]);
+        graphics.setCharacter(x + dx, y, chars.corners[1]);
+        graphics.setCharacter(x, y + dy, chars.corners[2]);
+        graphics.setCharacter(x + dx, y + dy, chars.corners[3]);
+    }
+
+    private static char getUnicode(int unicode) {
         return Character.toChars(unicode)[0];
     }
 
-    private void drawSquare(TerminalPosition position, TerminalSize size, SquareType type) {
-        // TODO
+    private static char[] getUnicode(int[] unicode) {
+        var chars = new char[unicode.length];
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = getUnicode(unicode[i]);
+        }
+        return chars;
     }
 }
