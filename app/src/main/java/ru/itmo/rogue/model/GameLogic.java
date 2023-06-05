df138384666cb0f74ec79c5cbf59323f3e8d76d9
Anelya Dusaeva <an.dusaeva+git@gmail.com>
2023-06-05 16:47:09 +0300

    hey now we have level logic!

--- app/src/main/java/ru/itmo/rogue/model/GameLogic.java
+++ app/src/main/java/ru/itmo/rogue/model/GameLogic.java
@@ -1,18 +1,38 @@
 package ru.itmo.rogue.model;
 
 import ru.itmo.rogue.control.Signal;
+import ru.itmo.rogue.model.game.LevelBuilder;
 import ru.itmo.rogue.model.game.UnitFactory;
 import ru.itmo.rogue.model.state.Delta;
 import ru.itmo.rogue.model.state.State;
+import ru.itmo.rogue.model.state.UnitUpdate;
 
 public class GameLogic {
 
-    private State state;
+    private final State state;
     public GameLogic(State state) {
         this.state = state;
     }
 
     public Delta update(Signal cmd) {
-        return null;
+        var delta = new Delta();
+        // map generation
+        var levelBuilder = new LevelBuilder();
+        state.levelMap = levelBuilder.build();
+        delta.setMap(state.levelMap);
+
+        // unit generation
+        state.units.add(state.player);
+
+        var unitFactory = new UnitFactory();
+        var numberOfEnemies = Math.log(state.player.getLevel());
+        for (int i = 0; i < numberOfEnemies; i++) {
+            var newMonster = unitFactory.getUnit();
+            state.units.add(newMonster);
+            delta.add(new UnitUpdate(newMonster));
+        }
+
+        state.focus = State.Focus.LEVEL;
+        return delta;
     }
 }
