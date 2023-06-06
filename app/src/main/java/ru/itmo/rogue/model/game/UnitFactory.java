7c7e0b1de7b5db5aa77fbc2a7cf51f7f2d444d6d
Lsr248 <i-fattakhova@mail.ru>
2023-06-06 22:08:07 +0300

    Added fixes

--- app/src/main/java/ru/itmo/rogue/model/game/UnitFactory.java
+++ app/src/main/java/ru/itmo/rogue/model/game/UnitFactory.java
@@ -7,11 +7,13 @@ import ru.itmo.rogue.model.game.unit.strategies.AgressiveStrategy;
 import ru.itmo.rogue.model.game.unit.strategies.CowardStrategy;
 import ru.itmo.rogue.model.game.unit.strategies.IdleStrategy;
 
-import java.util.HashMap;
 import java.util.Random;
 
 public class UnitFactory {
-    public static final char ALIVE_ENEMY = '*';
+    public static final char ALIVE_AGGRESSIVE_ENEMY = '*';
+    public static final char ALIVE_IDLE_ENEMY = '#';
+    public static final char ALIVE_COWARD_ENEMY = '%';
+
     public static final char DEAD_ENEMY = 'x';
     public static final char ALIVE_PLAYER = '@';
     public static final char DEAD_PLAYER = '?';
@@ -22,10 +24,8 @@ public class UnitFactory {
     public static int UNIT_HEALTH = 2;
     public static int UNIT_EXPERIENCE = 2;
     public static int UNIT_LEVEL = 2;
-    public static String IDLE_PROBABILITY = "idleProb";
-    public static String COWARD_PROBABILITY = "cowardProb";
-    public static String AGGRESSIVE_PROBABILITY = "aggressiveProb";
     private final Random random = new Random();
+
     public int complexity;
 
 
@@ -50,30 +50,19 @@ public class UnitFactory {
         if (complexity == 0) {
             return getIdleUnit();
         }
-        HashMap<String, Integer> probabilities = getProbabilities();
-        int sum = probabilities.values().stream().mapToInt(Integer::intValue).sum();
+        Probabilities probabilities = new Probabilities();
+        int sum = probabilities.sum();
 
         int num = random.nextInt(sum);
-        if (num < probabilities.get(IDLE_PROBABILITY)) {
+        if (num < probabilities.idleProb) {
             return getIdleUnit();
         }
-        if (num < probabilities.get(IDLE_PROBABILITY) + probabilities.get(COWARD_PROBABILITY)) {
+        if (num < probabilities.idleProb + probabilities.cowardProb) {
             return getCowardUnit();
         }
         return getAgressiveUnit();
     }
 
-    private HashMap<String, Integer> getProbabilities() {
-        HashMap<String, Integer> prob = new HashMap<>();
-        int idleProb = random.nextInt(20 / complexity);
-        int cowardProb = random.nextInt(20 / complexity) + idleProb;
-        int aggressiveProb = random.nextInt(20 * complexity) + cowardProb;
-        prob.put(IDLE_PROBABILITY, idleProb);
-        prob.put(COWARD_PROBABILITY, cowardProb);
-        prob.put(AGGRESSIVE_PROBABILITY, aggressiveProb);
-
-        return prob;
-    }
 
     private Unit getAgressiveUnit() {
         return new Unit(UNIT_HEALTH,
@@ -81,7 +70,7 @@ public class UnitFactory {
                 UNIT_EXPERIENCE,
                 UNIT_LEVEL,
                 new Position(),
-                new AgressiveStrategy(), ALIVE_ENEMY, DEAD_ENEMY);
+                new AgressiveStrategy(), ALIVE_AGGRESSIVE_ENEMY, DEAD_ENEMY);
     }
 
     private Unit getCowardUnit() {
@@ -90,7 +79,7 @@ public class UnitFactory {
                 UNIT_EXPERIENCE,
                 UNIT_LEVEL,
                 new Position(),
-                new CowardStrategy(), ALIVE_ENEMY, DEAD_ENEMY);
+                new CowardStrategy(), ALIVE_COWARD_ENEMY, DEAD_ENEMY);
     }
 
     private Unit getIdleUnit() {
@@ -99,7 +88,24 @@ public class UnitFactory {
                 UNIT_EXPERIENCE,
                 UNIT_LEVEL,
                 new Position(),
-                new IdleStrategy(), ALIVE_ENEMY, DEAD_ENEMY);
+                new IdleStrategy(), ALIVE_IDLE_ENEMY, DEAD_ENEMY);
+    }
+
+    class Probabilities {
+        private final int idleProb;
+        private final int cowardProb;
+        private final int aggressiveProb;
+
+        private Probabilities() {
+            this.idleProb = random.nextInt(20 / complexity);
+            this.cowardProb = random.nextInt(20 / complexity) + idleProb;
+            this.aggressiveProb = random.nextInt(20 * complexity) + cowardProb;
+        }
+
+        private int sum() {
+            return idleProb + cowardProb + aggressiveProb;
+        }
+
     }
 
 }
