0d17b342c6e2061197b65063bcbec327fd74483d
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-06-06 21:29:00 +0300

    Unit tests for map

--- app/src/test/java/ru/itmo/rogue/model/state/TestMap.java
+++ app/src/test/java/ru/itmo/rogue/model/state/TestMap.java
@@ -24,10 +24,36 @@ public class TestMap {
         assertSquareDistance(map, start, new Position(2, 2));
         assertSquareDistance(map, start, new Position(6, 6));
         assertSquareDistance(map, start, new Position(8, 8));
-//        assertEquals(15, map.getDistance(new Position(1, 1), new Position(9, 8)));
     }
 
-    public void assertSquareDistance(Map map, Position from, Position to) {
+    @Test
+    public void testUnreachable() {
+        Map map = new Map(10, 10, 0);
+
+        var start = new Position(1, 1);
+
+        // Should be unreachable (out of bounds):
+        assertEquals(-1, map.getDistance(start, new Position(14, 5)));
+
+        // Should be unreachable (walls)
+        assertEquals(-1, map.getDistance(start, new Position(14, 11)));
+        assertEquals(-1, map.getDistance(start, new Position(21, 5)));
+
+        // Should be REACHABLE:
+        assertNotEquals(-1, map.getDistance(start, new Position(10, 1)));
+        assertNotEquals(-1, map.getDistance(start, new Position(10, 5)));
+
+        // Place wall in the middle
+        for (int i = 0; i < map.getHeight(); i++) {
+            map.setTile(new Position(5, i), Map.MapTile.WALL);
+        }
+
+        // Should be unreachable
+        assertEquals(-1, map.getDistance(start, new Position(10, 1)));
+        assertEquals(-1, map.getDistance(start, new Position(10, 5)));
+    }
+
+    private void assertSquareDistance(Map map, Position from, Position to) {
         var distance = map.getDistance(from, to);
         var movement = from.getMovement(to);
         assertEquals(movement.getLength(), distance);
